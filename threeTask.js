c3ce8e051bb0cc1199665240dd64752f0fd603ba
hakamfaza <hakamfaza@gmail.com>
2022-03-16 07:30:53 +0700

    preparation

--- threeTask.js
+++ threeTask.js
@@ -1,35 +1,35 @@
-// const makeID = (nama, age) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const isAge = age > 17;
-//       if (isAge) {
-//         resolve(`Create ID Card ${nama}`)
-//       } else {
-//         reject('Still under seventeen years old!');
-//       }
-//     }, 3000)
+const makeID = (nama, age) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const isAge = age > 17;
+      if (isAge) {
+        resolve(`Create ID Card ${nama}`)
+      } else {
+        reject('Still under seventeen years old!');
+      }
+    }, 3000)
+  })
+}
+
+const main = async () => {
+  try {
+    console.log('Loading...');
+    const data = await makeID('Muhamad Hakam Faza', 19)
+    console.log(data)
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+main()
+
+// makeID('Muhamad Hakam Faza', 19)
+//   .then((res) => {
+//     console.log(res)
 //   })
-// }
-
-// const main = async () => {
-//   try {
-//     console.log('Loading...');
-//     const data = await makeID('Muhamad Hakam Faza', 19)
-//     console.log(data)
-//   }
-//   catch (err) {
+//   .catch((err) => {
 //     console.log(err)
-//   }
-// }
-// main()
-
-// // makeID('Muhamad Hakam Faza', 19)
-// //   .then((res) => {
-// //     console.log(res)
-// //   })
-// //   .catch((err) => {
-// //     console.log(err)
-// //   })
+//   })
 
 // const saying = (subject, sentence) => {
 //   return new Promise((resolve, reject) => {
